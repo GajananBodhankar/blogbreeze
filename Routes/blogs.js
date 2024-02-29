cc1a763d7c68895c3ddb8252299b68cb28c3817c
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-29 22:43:26 +0530

    made a change

--- Routes/blogs.js
+++ Routes/blogs.js
@@ -6,7 +6,7 @@ let route = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images");
+    cb(null, "./public/images");
   },
   filename: (req, file, cb) => {
     cb(
