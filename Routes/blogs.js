073fc23a93665c3388b93d7b0820d4af1d1e5504
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-03-01 13:14:12 +0530

    made a change

--- Routes/blogs.js
+++ Routes/blogs.js
@@ -3,64 +3,55 @@ import blogModel from "../model.js";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
-let __filename = fileURLToPath(import.meta.url);
-let __dirname = path.dirname(__filename);
+// let __filename = fileURLToPath(import.meta.url);
+// let __dirname = path.dirname(__filename);
 let route = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'public/images'));
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, path.join(__dirname, 'public/images'));
+//   },
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
 
-const upload = multer({
-  storage: storage,
-});
+// const upload = multer({
+//   storage: storage,
+// });
 
 // To upload a blog
-route.post(
-  "/:username",
-  upload.fields([
-    { name: "image", maxCount:100 },
-    { name: "title", maxCount: 100 },
-    { name: "content", maxCount:100 },
-    { name: "related_links", maxCount: 100 },
-  ]),
-  async (req, res) => {
-    let user = req.params.username;
-    let data = await blogModel.find({ username: user });
-    let postBlog = {
-      ...req.body,
-      related_links: JSON.parse(req.body.related_links),
-      image: req.files?.image[0].filename,
-    };
-    let all = data[0].blogs;
-    all.push(postBlog);
-    let response = await blogModel.updateOne(
-      { username: user },
-        {
-          $set: {
-            blogs: all,
-          },
-        }
-      );
-      if (response.acknowledged) {
-        res.send({
-          success: true,
-          message: "Blog uploaded successfully",
-          id: data[0].blogs[data[0].blogs.length - 1]._id,
-        });
-      } else {
-        res.send({ success: false, message: "Uploading failed" });
-      }
+route.post("/:username", async (req, res) => {
+  let user = req.params.username;
+  let data = await blogModel.find({ username: user });
+  let postBlog = {
+    ...req.body,
+    related_links: JSON.parse(req.body.related_links),
+    image: req.body.image,
+  };
+  let all = data[0].blogs;
+  all.push(postBlog);
+  let response = await blogModel.updateOne(
+    { username: user },
+    {
+      $set: {
+        blogs: all,
+      },
     }
   );
+  if (response.acknowledged) {
+    res.send({
+      success: true,
+      message: "Blog uploaded successfully",
+      id: data[0].blogs[data[0].blogs.length - 1]._id,
+    });
+  } else {
+    res.send({ success: false, message: "Uploading failed" });
+  }
+});
 
 // To delete a blog
 route.delete("/delete/:username/:id", async (req, res) => {
