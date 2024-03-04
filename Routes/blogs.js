e7139a5fa246e8cf582a2f4107037638d5a545d7
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-03-04 14:20:19 +0530

    Made a chnage

--- Routes/blogs.js
+++ Routes/blogs.js
@@ -155,17 +155,6 @@ route.put("/likes/:username/:blogId", async (req, res) => {
   res.send({ success: true, id: response[0]._id, data: update });
 });
 
-route.get("/favorite/:username", async (req, res) => {
-  let username = req.params.username;
-  let data = await blogModel.find({ username: username });
-  let response = data[0].favorites;
-  if (data.length > 0) {
-    res.status(200).send(response);
-  } else {
-    res.send({ success: false, message: "No blogs added to favorite" });
-  }
-});
-
 route.get("/user/:username", async (req, res) => {
   let user = req.params.username;
   let data = await blogModel.find({ username: user });
